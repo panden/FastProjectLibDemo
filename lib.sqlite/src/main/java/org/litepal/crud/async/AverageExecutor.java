f771f16105c1bd5d48a0b8f47ef7f482344dca46
siwei <studyforandroid@gmail.com>
2018-05-03 23:46:01 +0800

    加密数据库代码(LitePal + SQLCipher)

---
+++ lib.sqlite/src/main/java/org/litepal/crud/async/AverageExecutor.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C)  Tony Green, LitePal Framework Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.litepal.crud.async;
+
+/**
+ * Executor for average query in background.
+ *
+ * @author Tony Green
+ * @since 2017/2/22
+ */
+public class AverageExecutor extends AsyncExecutor {
+
+    private org.litepal.crud.callback.AverageCallback cb;
+
+    /**
+     * Register a callback listener and async task will start executing right away.
+     * @param callback
+     *          Callback for average query in background.
+     */
+    public void listen(org.litepal.crud.callback.AverageCallback callback) {
+        cb = callback;
+        execute();
+    }
+
+    public org.litepal.crud.callback.AverageCallback getListener() {
+        return  cb;
+    }
+
+}
